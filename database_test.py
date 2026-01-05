689063637b5fbb311ff03d2fd8fe76f6df334abd
jasman <jasman0603@gmail.com>
2026-01-05 14:00:22 +0700

    database initialization

--- database_test.py
+++ database_test.py
@@ -5,20 +5,25 @@ from urllib.parse import quote_plus
 
 load_dotenv()
 
-USER = os.getenv("user")
-PASSWORD = quote_plus(os.getenv("password"))  # ⬅️ penting
-HOST = os.getenv("host")
-PORT = os.getenv("port")
-DBNAME = os.getenv("dbname")
+USER = os.getenv("DB_USER")
+PASSWORD = quote_plus(os.getenv("DB_PASSWORD"))
+HOST = os.getenv("DB_HOST")
+PORT = os.getenv("DB_PORT")
+DBNAME = os.getenv("DB_NAME")
 
 DATABASE_URL = (
-    f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
+    f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}"
 )
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"sslmode": "require"},
+    pool_pre_ping=True
+)
 
 try:
-    with engine.connect() as connection:
+    with engine.connect() as conn:
+        conn.execute("SELECT 1")
         print("✅ Connection successful!")
 except Exception as e:
     print("❌ Failed to connect:", e)
